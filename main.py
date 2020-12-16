6453296c8c13482c4f7dd3c1a787e400ab7da187
Shantanu Ghodgaonkar <69143889+shantanu-ghodgaonkar@users.noreply.github.com>
2020-12-16 16:28:54 +0530

    Update 4 - Created list of images

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ root = Tk()
 root.title('Tkinter Image Viewer')
 root.iconbitmap('S:/Study+Work/Python/TkinterTester/icons/icons8-shark-64.ico')
 
+img1 = ImageTk.PhotoImage(Image.open('S:/Study+Work/Python/TkinterTester/images/IA1.jpg'))
+img2 = ImageTk.PhotoImage(Image.open('S:/Study+Work/Python/TkinterTester/images/IA2.jpg'))
+img3 = ImageTk.PhotoImage(Image.open('S:/Study+Work/Python/TkinterTester/images/IA3.jpg'))
+img4 = ImageTk.PhotoImage(Image.open('S:/Study+Work/Python/TkinterTester/images/IA4.jpg'))
+img5 = ImageTk.PhotoImage(Image.open('S:/Study+Work/Python/TkinterTester/images/IA5.jpg'))
+
+img_list = [img1, img2, img3, img4, img5]
 btn_exit = Button(root, text='EXIT', command=root.quit)
 btn_exit.pack()
 
