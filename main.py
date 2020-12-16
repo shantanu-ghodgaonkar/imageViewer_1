fc9d927db8365573f3569c2541991b2a558d1fd5
Shantanu Ghodgaonkar <69143889+shantanu-ghodgaonkar@users.noreply.github.com>
2020-12-16 17:05:27 +0530

    Update 7 - Button commands created

--- main.py
+++ main.py
@@ -12,23 +12,41 @@ img4 = ImageTk.PhotoImage(Image.open('C:/Users/shant/Downloads/Album Covers/A Da
 img5 = ImageTk.PhotoImage(Image.open('C:/Users/shant/Downloads/Album Covers/Youthanasia_Megadeth.jpg'))
 
 img_list = [img1, img2, img3, img4, img5]
-img_lbl = Label(root, image=img_list[1])
+img_lbl = Label(root, image=img_list[0])
 img_lbl.grid(row=0, column=0, columnspan=3)
 
 
-def nextImage():
+def nextImage(img_num: int):
     global img_lbl, btn_next, btn_prev
+    img_lbl.grid_forget()
+    img_lbl = Label(root, image=img_list[img_num - 1])
+    btn_next = Button(root, text='>>', command=lambda: nextImage(img_num+1))
+    btn_prev = Button(root, text='<<', command=lambda: prevImage(img_num-1))
+    if img_num == 5:
+        btn_next = Button(root, text='>>', state=DISABLED)
+    img_lbl.grid(row=0, column=0, columnspan=3)
+    btn_next.grid(row=1, column=2)
+    btn_prev.grid(row=1, column=0)
 
 
-def prevImage():
+def prevImage(img_num: int):
     global img_lbl, btn_next, btn_prev
+    img_lbl.grid_forget()
+    img_lbl = Label(root, image=img_list[img_num-1])
+    btn_next = Button(root, text='>>', command=lambda: nextImage(img_num+1))
+    btn_prev = Button(root, text='<<', command=lambda: prevImage(img_num-1))
+    if img_num == 1:
+        btn_prev = Button(root, text='<<', state=DISABLED)
+    img_lbl.grid(row=0, column=0, columnspan=3)
+    btn_next.grid(row=1, column=2)
+    btn_prev.grid(row=1, column=0)
 
 
 btn_exit = Button(root, text='EXIT', command=root.quit)
 btn_exit.grid(row=1, column=1)
 btn_next = Button(root, text='>>', command=lambda: nextImage(2))
 btn_next.grid(row=1, column=2)
-btn_prev = Button(root, text='<<', command=lambda: prevImage)
+btn_prev = Button(root, text='<<', command=prevImage, state=DISABLED)
 btn_prev.grid(row=1, column=0)
 
 if __name__ == '__main__':
