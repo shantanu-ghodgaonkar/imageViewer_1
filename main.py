2e3239a0a59eeeeb9a4f6a8497e2188008e5c179
Shantanu Ghodgaonkar <69143889+shantanu-ghodgaonkar@users.noreply.github.com>
2020-12-16 16:22:33 +0530

    Update 2 - Added EXIT button

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ root = Tk()
 root.title('Tkinter Image Viewer')
 root.iconbitmap('S:/Study+Work/Python/TkinterTester/icons/icons8-shark-64.ico')
 
+btn_exit = Button(root, text='EXIT', command=root.quit)
+btn_exit.pack()
 
 if __name__ == '__main__':
     root.mainloop()
